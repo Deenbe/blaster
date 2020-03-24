fabeb92e33c8f7c449803ad2395951858e2541a2
Buddhike de Silva <buddyspike@geeksdiary.com>
2020-03-24 21:03:36 +1100

    Improved test with more assertions

--- lib/message_pump_test.go
+++ lib/message_pump_test.go
@@ -1,32 +1,31 @@
 package lib
 
 import (
-	"sync"
 	"testing"
 	"time"
 
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBasicMessageDispatch(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	var wg sync.WaitGroup
-	wg.Add(1)
 
 	q := NewMockQueueService(ctrl)
 	m := new(Message)
 	q.EXPECT().Read().Return([]*Message{m}, nil)
-	q.EXPECT().Delete(gomock.Any()).Return(nil)
+	q.EXPECT().Delete(m).Return(nil)
 
 	d := NewMockDispatcher(ctrl)
+	observe := make(chan *Message)
 	d.EXPECT().Dispatch(gomock.Any()).Do(func(m *Message) error { 
-		wg.Done() 
+		observe <- m
 		return nil
 	})
 
 	p := NewMessagePump(q, d, 0, time.Second)
 	p.pumpMain()
 
-	wg.Wait()
+	assert.Equal(t, m, <-observe)
 }
