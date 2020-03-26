4a20e511a28d3feadc988a23f4bc53a6b875aafb
Buddhike de Silva <buddyspike@geeksdiary.com>
2020-03-26 18:00:35 +1100

    Message pump tests

--- lib/message_pump_test.go
+++ lib/message_pump_test.go
@@ -2,7 +2,9 @@ package lib
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 
@@ -37,7 +39,7 @@ func TestBasicMessageDispatch(t *testing.T) {
 	<-p.Done
 }
 
-func TestMaxMessageHandlers(t *testing.T) {
+func TestMaxMessageHandlersWithoutBuffering(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -75,4 +77,90 @@ func TestMaxMessageHandlers(t *testing.T) {
 	for _, e := range expectedOrderOfEvents {
 		assert.Equal(t, e, <-events)
 	}
+}
+
+func TestMaxMessageHandlersWithBuffering(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	events := make(chan string)
+
+	q := NewMockQueueService(ctrl)
+	m1 := &Message{MessageID: "m1"}
+	m2 := &Message{MessageID: "m2"}
+	q.EXPECT().Read().DoAndReturn(func() ([]*Message, error) {
+		events <- "read m1 and m2"
+		return []*Message{m1, m2}, nil
+	})
+	q.EXPECT().Read().Return([]*Message{}, nil).AnyTimes()
+	q.EXPECT().Delete(gomock.Any()).DoAndReturn(func(m *Message) error {
+		events <- fmt.Sprintf("delete %s", m.MessageID)
+		return nil
+	}).AnyTimes()
+
+	d := NewMockDispatcher(ctrl)
+	d.EXPECT().Dispatch(gomock.Any()).Do(func(m *Message) error {
+		events <- fmt.Sprintf("dispatch %s", m.MessageID)
+		return nil
+	}).AnyTimes()
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+	p := NewMessagePump(q, d, 0, time.Second, 1)
+	p.Start(ctx)
+
+	expectedOrderOfEvents := []string{"read m1 and m2", "dispatch m1", "delete m1", "dispatch m2", "delete m2"}
+	for _, e := range expectedOrderOfEvents {
+		assert.Equal(t, e, <-events)
+	}
+}
+
+func TestErrorOnQueueServiceRead(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	done := make(chan struct{})
+
+	q := NewMockQueueService(ctrl)
+	q.EXPECT().Read().Return([]*Message{}, errors.New("doh"))
+	q.EXPECT().Read().DoAndReturn(func() ([]*Message, error) {
+		done <- struct{}{}
+		return []*Message{}, nil
+	}).AnyTimes()
+
+	d := NewMockDispatcher(ctrl)
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+	p := NewMessagePump(q, d, 0, time.Second, 1)
+	p.Start(ctx)
+
+	<-done
+}
+
+func TestCancelationWhileWaitingForDispatcherToReturn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	q := NewMockQueueService(ctrl)
+	m1 := &Message{MessageID: "m1"}
+	q.EXPECT().Read().Return([]*Message{m1}, nil)
+	q.EXPECT().Read().Return([]*Message{}, nil).AnyTimes()
+	q.EXPECT().Delete(gomock.Any()).Return(nil).AnyTimes()
+
+	d := NewMockDispatcher(ctrl)
+	d.EXPECT().Dispatch(gomock.Any()).Do(func(m *Message) error {
+		wg.Wait()
+		return nil
+	}).AnyTimes()
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	p := NewMessagePump(q, d, 0, time.Second, 1)
+	p.Start(ctx)
+
+	cancelFunc()
+	<-p.Done
 }
