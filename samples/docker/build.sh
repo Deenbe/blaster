d1f1dc2b96e57071d02a2f50031e5ff8d055bd8d
Buddhike de Silva <buddyspike@geeksdiary.com>
2020-03-27 17:46:42 +1100

    Docker sample

---
+++ samples/docker/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -d .tmp ]
+then
+    mkdir .tmp
+fi
+
+if [ ! -f .tmp/blaster ]
+then
+curl -L -o .tmp/blaster https://github.com/buddyspike/blaster/releases/download/v0.1.12/blaster_linux_amd64
+chmod +x .tmp/blaster
+fi
+
+docker build -t blaster-handler .
---
+++ samples/docker/index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node 
+
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    console.log(`${req.body.messageId}: ${req.body.body}`);
+    return res.send('ok');
+});
+
+// By default blaster forwards messages to http://localhost:8312
+app.listen(8312, () => {
+    console.log('listening');
+});
