a7c207d3f9c3082f9ef30b92480eb4e613d175b5
Buddhike de Silva <buddyspike@geeksdiary.com>
2020-03-29 19:52:18 +1100

    Use a readiness check in docker sample

--- samples/docker/index.js
+++ samples/docker/index.js
@@ -5,12 +5,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => res.send('ok'));
+app.post('/', (req, res) => { 
     console.log(`${req.body.messageId}: ${req.body.body}`);
     return res.send('ok');
 });
 
 // By default blaster forwards messages to http://localhost:8312
-app.listen(8312, () => {
-    console.log('listening');
-});
+app.listen(8312, () => { console.log('listening'); });
